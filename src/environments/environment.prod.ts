d8041767bc67391eda5e23d15912d45b34cdb0ca
Eduardo Viana Pessoa <eduardo.vianap@gmail.com>
2019-11-08 14:39:43 -0700

    First commit.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCJp985flakYr1ijVMPDCvEU2aVVMRq_lE',
+    authDomain: 'lumos-tv.firebaseapp.com',
+    databaseURL: 'https://lumos-tv.firebaseio.com',
+    projectId: 'lumos-tv',
+    storageBucket: 'lumos-tv.appspot.com',
+    messagingSenderId: '280702226503'
+  }
+};
